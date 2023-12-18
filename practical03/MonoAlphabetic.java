e41aa51082196946d2f0be5eef0c76d3913d104d
Shridhar Pandey <63766338+IAmShridharr@users.noreply.github.com>
2023-12-18 18:47:07 +0530

    Update MonoAlphabetic.java

--- practical03/MonoAlphabetic.java
+++ practical03/MonoAlphabetic.java
@@ -10,11 +10,11 @@ public class MonoAlphabetic {
         StringBuilder output = new StringBuilder();
         Scanner sc = new Scanner(System.in);
         String plainText = "abcdefghijklmnopqrstuvwxyz";
-        String cipherText = "GHDWVFJEBDHVYUCD";
+        String cipherText = "GHDWVFJEBDHVYUCDISLKJPOQA"; // This line was corrected by Avanishchandra Yadav
         System.out.println("PlainText: " + plainText);
         System.out.println("CipherText: " + cipherText);
         System.out.println("Using MonoAlphabetic: ");
-        System.out.print("Enter Data To Send: "); // college
+        System.out.print("Enter Data To Send: "); // e.g. avanish (Uppercase characters are not allowed)
         input = sc.nextLine();
         for(i = 0; i < input.length(); i++) {
             ch = input.charAt(i);
